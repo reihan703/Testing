48f2bda1566b284908e76c5beb2ab2c8c7fbfa13
Reihan <reihanyasfinu@gmail.com>
2022-01-04 10:17:23 +0700

    Initial

---
+++ backend/Interest.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(){
+	double j, k;
+	int i;
+	j=10000;
+	i=1;
+	
+	while (k<=20000){
+		j=(j*0.05)+j;
+			printf("Tahun ke %d adalah %.2f\n",i,j);
+			k=j;
+			i++;
+	}
+}
+	
+
---
+++ frontend/rent.cpp
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    char type;
+    float hour;
+    printf("What type of car? Pick the initial\nSedan(S)\nLuxury(L)\nMPV(M)\nYour choice is: ");
+    scanf("%c", &type);
+    printf("\nHow long will you rent it (Hour)? ");
+    scanf("%f", &hour);
+    
+    if('S'==type||'s'==type){
+        float rate = 20.00;
+        if(hour>18){
+            float total = rate*hour;
+            float discount = total-total*10/100;
+            printf("%.2f", discount);
+            exit(0);
+        }
+        float total = rate*hour;
+        printf("%.2f", total);
+    }
+    
+    if('L'==type||'l'==type){
+        float rate = 35.00;
+        if(hour>18){
+            float total = rate*hour;
+            float discount = total-total*15/100;
+            printf("%.2f", discount);
+            exit(0);
+        }
+        float total = rate*hour;
+        printf("%.2f", total);
+    }
+    
+    if('M'==type||'m'==type){
+        float rate = 45.00;
+        if(hour>18){
+            float total = rate*hour;
+            float discount = total-total*20/100;
+            printf("%.2f", discount);
+            exit(0);
+        }
+        float total = rate*hour;
+        printf("%.2f", total);
+    }
+}
